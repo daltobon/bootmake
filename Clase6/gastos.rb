e5bd086a7c15fd1f4b04d4b535144a0005ec8b92
daltobon <daniel18.tobon@gmail.com>
2018-02-27 14:33:55 -0500

    Archivos contenidos en Clase6

---
+++ Clase6/gastos.rb
@@ -0,0 +1,45 @@
+require 'date'
+require_relative "./interfaz.rb"
+
+class Costos
+
+ attr_accessor :ingresos, :egreso, :fecha
+
+  def initialize
+      @ingresos = []
+  end
+
+  #Metodo para Ingresos
+    def ingresos(cantidad, fecha, descripcion)
+       arrfecha = fecha.split("-").map {|c| c.to_i}
+       fechafinal = Date.new(arrfecha[0], arrfecha[1], arrfecha[2])
+       @ingresos << {cantidad: cantidad, fecha: fecha, descripcion: descripcion }
+       if cantidad <= 0
+       puts "No tiene ingresos"
+       else
+       puts "Ingreso exitoso"
+       end
+    end
+
+    def egresos(cantidad, fecha, descripcion)
+       arrfecha = fecha.split("-").map {|c| c.to_i}
+       fechafinal = Date.new(arrfecha[0], arrfecha[1], arrfecha[2])
+       @ingresos << {cantidad: cantidad, fecha: fecha, descripcion: descripcion }
+       if cantidad <= 0
+       puts "No tiene ingresos"
+       else
+       puts "Ingreso exitoso"
+       end
+    end
+
+
+end
+
+
+
+
+
+
+g1 = Costos.new
+hola = g1.ingresos(0, "2018-02-26", "Servicios")
+puts hola
