6510329eaeaabe75291d02d9c684f4c4a70dadf3
daltobon <daniel18.tobon@gmail.com>
2018-02-28 08:29:29 -0500

    Borrado

--- Clase7/gastos.rb
+++ Clase7/gastos.rb
@@ -1,13 +1,10 @@
 require 'date'
-
 class Costos
- attr_accessor :ingresos, :egresos, :total
+ attr_accessor :ingresos, :egreso, :mes
 
   def initialize
       @ingresos = []
-      @egresos = 0
-      @mes = 0
-      @total = 0
+      @mes = (1..12)
   end
 
   #Metodo para Ingresos
@@ -17,43 +14,28 @@ class Costos
        @ingresos << {cantidad: cantidad, fecha: fechafinal, descripcion: descripcion }
     end
 
-  #Metodo para Egresos
     def egresos(cantidad, fecha, descripcion)
        arrfecha = fecha.split("-").map {|c| c.to_i}
        fechafinal = Date.new(arrfecha[0], arrfecha[1], arrfecha[2])
-       binding.pry
        @ingresos << {cantidad: cantidad, fecha: fechafinal, descripcion: descripcion }
     end
 
-  #Metdo para meses
     def saldo_mes(mes)
-        @ingresos.each do |ingreso|
-           if mes == ingreso[:fecha].month
-           puts "Tus ingresos en el mes #{mes} fueron de #{ingreso[:cantidad]}"
+        @ingresos.each do |element|
+          if mes == element[:fecha].month
+           puts "#{element[:cantidad]}"
           else
-            puts "No tienes ingresos en el mes #{mes}"
+            puts "No tienes ingresos en ese mes"
           end
         end
-    end
-
-    def total(ingresototal, mes)
-        @ingresos.each do |ingreso|
-            @egresos.each do |egreso|
-      if mes == ingreso[:fecha].month && egreso[:fecha].month
-        ingresototal =
-
-
       end
-    end
-end
+   end
+
+
 
 g1 = Costos.new
-servicios = g1.ingresos(9000, "2018-02-08", "Servicios")
-
-puts servicios
+hola = g1.ingresos(990, "2018-02-26", "Servicios")
+puts hola
 
 saldo = g1.saldo_mes(2)
 puts saldo
-
-saldototal = g1.total(2)
-puts saldototal
