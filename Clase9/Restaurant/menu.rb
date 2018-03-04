155d5775c88f686ca08a77d18adff455d6d0b80d
daltobon <daniel18.tobon@gmail.com>
2018-03-04 18:28:00 -0500

    Metodo para agregar acrchivos

--- Clase9/Restaurant/menu.rb
+++ Clase9/Restaurant/menu.rb
@@ -3,8 +3,8 @@
  Metodo que permita agregar elementos al menu_comidas
  Metodo que permita consultar un producto por codigo
 =end
-
 require 'pry'
+
 module Menu
 
     class Clase_menu
@@ -15,27 +15,28 @@ module Menu
           end
 
           def principal
-                comidas = File.foreach("productos.csv").grep(/00/)
-                comidas.each do |element|
-                  item = element.split(',')
-                  comida = { valor: item[2], nombre: item[1], stock: item[3] }
-                  @comidas[item[0]] = comida
-              end
-           end
-
-           def menu
-             puts @comidas
-           end
-
-          def consultar(codigo)
-             comida = @comidas[codigo]
-               puts "#{comida[:valor]} -- #{comida[:nombre]} --- #{comida[:stock]}"
+            comidas = File.foreach("productos.csv").grep(/00/)
+              comidas.each do |element|
+            item = element.split(',')
+              comida = { valor: item[2], nombre: item[1], stock: item[3] }
+            @comidas[item[0]] = comida
             end
           end
 
+          def agregar(elemento)
+              File.open("productos.csv", 'a+') {|f| f.write(elemento.to_s)}
+          end
+
+          def consultar(codigo)
+            if comida = @comidas[codigo]
+              puts "#{comida[:valor]}      #{comida[:nombre]}      #{comida[:stock]}"
+            else
+              puts "No existe en el menú"
+            end
+          end
+      end
   end
 
 a = Menu::Clase_menu.new
 a.principal
-a.menu
-a.consultar(gets.chomp)
+a.agregar("V006;Casa;4000;true") 
