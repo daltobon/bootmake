319858d4cc97826eb89d61c6f93108b3532d46ae
daltobon <daniel18.tobon@gmail.com>
2018-03-04 15:35:59 -0500

    Nuevos cambios

--- Clase9/Restaurant/menu.rb
+++ Clase9/Restaurant/menu.rb
@@ -8,34 +8,34 @@ require 'pry'
 module Menu
 
     class Clase_menu
-      attr_accessor :comidas, :codigo, :nombre, :valor, :stok, :productos
+      attr_accessor :comidas
 
-      def initialize
-        @comidas = Hash.new
-        @productos = Hash.new
-
-      end
-
-        def principal
-            num_comidas = 0
-              comidas = File.foreach("productos.csv").grep(/00/)
-              comidas.each do |element|
-                item = element.split(',')
-                comida = { valor: item[2], nombre: item[1], stock: item[3] }
-                @comidas[item[0]] = comida
-              num_comidas += 1
-            end
-            puts @comidas["C001"]
-            end
-
-          def consultar
-
+          def initialize
+            @comidas = Hash.new
           end
 
+          def principal
+                comidas = File.foreach("productos.csv").grep(/00/)
+                comidas.each do |element|
+                  item = element.split(',')
+                  comida = { valor: item[2], nombre: item[1], stock: item[3] }
+                  @comidas[item[0]] = comida
+              end
+           end
 
+           def menu
+             puts @comidas
+           end
 
-       end
+          def consultar(codigo)
+             comida = @comidas[codigo]
+               puts "#{comida[:valor]} -- #{comida[:nombre]} --- #{comida[:stock]}"
+            end
+          end
+
   end
-a = Menu::Clase_menu.new.principal
-# l  = a.menu_comidas("C001")
-# puts l
+
+a = Menu::Clase_menu.new
+a.principal
+a.menu
+a.consultar(gets.chomp)
