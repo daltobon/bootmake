4f5367ddd817e6a9c9c773b5a404999729a4917d
daltobon <daniel18.tobon@gmail.com>
2018-03-05 23:23:31 -0500

    Modificaciones medtodo agregar

--- Clase9/Restaurant/menu.rb
+++ Clase9/Restaurant/menu.rb
@@ -8,35 +8,51 @@ require 'pry'
 module Menu
 
     class Clase_menu
-      attr_accessor :comidas
+      attr_accessor :comidas, :new_producto
 
           def initialize
             @comidas = Hash.new
+            @codigo = 0
           end
 
           def principal
             comidas = File.foreach("productos.csv").grep(/00/)
-              comidas.each do |element|
+            comidas.each do |element|
             item = element.split(',')
-              comida = { valor: item[2], nombre: item[1], stock: item[3] }
+            comida = {codigo: item[0], valor: item[2.to_i], nombre: item[1], stock: item[3] }
             @comidas[item[0]] = comida
+            item[0] = @codigo
             end
           end
 
-          def agregar(elemento)
-              File.open("productos.csv", 'a+') {|f| f.write(elemento.to_s)}
+          def agregar(new_codigo)
+            if @comidas =  new_codigo
+             puts "El producto ya existe por favor verifique"
+            else element = gets.chomp
+              File.open("productos.csv", 'a+') {|f| f.write(element.to_s)}
+            end
           end
 
           def consultar(codigo)
             if comida = @comidas[codigo]
-              puts "#{comida[:valor]}      #{comida[:nombre]}      #{comida[:stock]}"
+              puts "#{comida[:valor].to_i}      #{comida[:nombre]}      #{comida[:stock]}"
             else
               puts "No existe en el menú"
             end
           end
+
+          def imprimir(codigo_new)
+
+            if codigo_old = codigo_new
+              puts "El producto ya existe por favor verifique"
+             else element = gets.chomp
+               File.open("productos.csv", 'a+') {|f| f.write(element.to_s)}
+             end
+           end
+
       end
   end
 
 a = Menu::Clase_menu.new
 a.principal
-a.agregar("V006;Casa;4000;true") 
+a.imprimir("C0202")
---
+++ Clase9/arr.rb
@@ -0,0 +1,1 @@
+arr = {"C001"=>{:codigo=>"C001", :valor=>"25000", :nombre=>"Lasagna", :stock=>"true\r\n"}}
